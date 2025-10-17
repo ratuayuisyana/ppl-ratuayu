e74d16ae2a7008160479b8c847f64e1adb8ea91b
ratuayuisyana <ratuayuisya@gmail.com>
2025-10-17 14:44:47 +0700

    rest-client2

---
+++ integration-test-ratu/testlab.js
@@ -0,0 +1,14 @@
+pm . test("Status code is 200", function (){
+    pm.response.to.have.status(200);
+});
+
+pm.test("Each Articlehas required fields", function(){
+    const jsonData = pm.response.json();
+
+    jsonData.articles.forEach(article => {
+        pm.expect(article).to.have.property("title");
+        pm.expect(article).to.have.property("description");
+        pm.expect(article).to.have.property("url");
+
+    });
+});
